91a84f351e7b907f0cb5e5bcde91375ea7c706bd
Krozark <maxime.barbier1991@gmail.com>
2021-08-11 04:25:29 +0200

    update setup error

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setuptools.setup(
    classifiers=[
       "Programming Language :: Python",
       "Programming Language :: Python :: 3",
-      "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
       "Operating System :: OS Independent",
     ],
    python_requires='>=3.6',
